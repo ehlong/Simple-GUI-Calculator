f09d8f391cc7bc67223159d14995e1991aab16e4
elliott.long <elliott.long@wsu.edu>
2020-04-28 01:11:53 -0700

    Calculator Functioning (With Reservations)
    Calculator can do most of its functions. Next is to add handling
    for decimal numbers.

--- calcwindow.py
+++ calcwindow.py
@@ -25,14 +25,79 @@ class CalcWindow(QMainWindow):
 		self.ui.equals.clicked.connect(self.buttonPressed)
 		self.ui.ac.clicked.connect(self.buttonPressed)
 		self.ui.neg.clicked.connect(self.buttonPressed)
+		self.plus = 0
+		self.minus = 0
+		self.equals = 0
+		self.div = 0
+		self.mul = 0
+		self.hold1 = 0
+		self.hold2 = 0
+
 
 	def buttonPressed(self):
 		clicked = self.sender()
-		if (clicked == 'one' or 'two' or 'three' or 'four' or \
-		'five' or 'six' or 'seven' or 'eight' or 'nine' or 'zero'):
+		numbers = [clicked == self.ui.one,
+					clicked == self.ui.two,
+					clicked == self.ui.three,
+					clicked == self.ui.four,
+					clicked == self.ui.five,
+					clicked == self.ui.six,
+					clicked == self.ui.seven,
+					clicked == self.ui.eight,
+					clicked == self.ui.nine,
+					clicked == self.ui.zero]
+		if any(numbers):
 			if self.ui.label.text() == "0":
 				self.ui.label.setText('')
 			self.ui.label.setText(self.ui.label.text() + clicked.text())
-		# if clicked == 'plus':
-
-
+		if clicked == self.ui.plus:
+			self.plus = 1
+			hold = self.ui.label.text()
+			self.hold1 = int(hold)
+		if clicked == self.ui.minus:
+			self.minus = 1
+			hold = self.ui.label.text()
+			self.hold1 = int(hold)
+		if clicked == self.ui.mul:
+			self.mul = 1
+			hold = self.ui.label.text()
+			self.hold1 = int(hold)
+		if clicked == self.ui.div:
+			self.div = 1
+			hold = self.ui.label.text()
+			self.hold1 = int(hold)
+		if clicked == self.ui.equals:
+			if self.plus == 1:
+				self.plus = 0
+				hold = self.ui.label.text()
+				self.hold2 = int(hold)
+				self.ui.label.setText(str(self.hold1 + self.hold2))
+			if self.minus == 1:
+				self.minus = 0
+				hold = self.ui.label.text()
+				self.hold2 = int(hold)
+				self.ui.label.setText(str(self.hold1 - self.hold2))
+			if self.mul == 1:
+				self.mul = 0
+				hold = self.ui.label.text()
+				self.hold2 = int(hold)
+				self.ui.label.setText(str(self.hold1 * self.hold2))
+			if self.div == 1:
+				self.div = 0
+				hold = self.ui.label.text()
+				self.hold2 = int(hold)
+				self.ui.label.setText(str(self.hold1 // self.hold2) + '.' \
+										+ str(self.hold1 % self.hold2))
+		if clicked == self.ui.ac:
+			self.plus = 0
+			self.minus = 0
+			self.equals = 0
+			self.div = 0
+			self.mul = 0
+			self.hold1 = 0
+			self.hold2 = 0
+			self.ui.label.setText('0')
+		if clicked == self.ui.neg:
+			hold = self.ui.label.text()
+			self.hold1 = float(hold)
+			self.ui.label.setText(str(self.hold1 * -1))
